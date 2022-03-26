147611e05e8ad73adaef477335c68265edde4600
Olga <olga@gmail.com>
2022-03-26 19:05:38 +0300

    feat: finished 'footer' block, added burger menu, added anchor links, fix: fixed picture sizes in 'our-work' block

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,15 @@ for (let button of buttons) {
         event.currentTarget.parentNode.nextElementSibling.classList.toggle('text-blocks__text--hidden');
     })
 }
+
+let burger = document.querySelector('.burger')
+burger.addEventListener('click', function (event) {
+    event.currentTarget.nextElementSibling.classList.toggle('nav__disabled');
+    document.querySelector('.sections-list').classList.toggle('disabled');
+})
+
+let close = document.querySelector('.close')
+close.addEventListener('click', function (event) {
+    event.currentTarget.parentNode.parentNode.classList.toggle('nav__disabled');
+    document.querySelector('.sections-list').classList.toggle('disabled');
+})
